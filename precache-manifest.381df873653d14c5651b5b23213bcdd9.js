91025b81754f9990462fe9c8cdf2d4a3cae6e0d8
nikitaHero <46008598+nikitaHero@users.noreply.github.com>
2020-05-31 15:08:09 +0300

    initiliaze app

---
+++ precache-manifest.381df873653d14c5651b5b23213bcdd9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b54a295620b6712590fb94a39faf0f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f49748f3da551ec96f9",
+    "url": "/static/css/main.be2309ea.chunk.css"
+  },
+  {
+    "revision": "9cff1bef4533f6992477",
+    "url": "/static/js/2.f17ecd66.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/static/js/2.f17ecd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f49748f3da551ec96f9",
+    "url": "/static/js/main.08f6def4.chunk.js"
+  },
+  {
+    "revision": "b93593463d87bc0665e8",
+    "url": "/static/js/runtime-main.1f0f2abf.js"
+  },
+  {
+    "revision": "70ca70dfdf6d1209cdb9325e24668e44",
+    "url": "/static/media/arrow.70ca70df.svg"
+  },
+  {
+    "revision": "b42582ce99a72934da09b98db1f6d2a7",
+    "url": "/static/media/bigslider_1.b42582ce.png"
+  },
+  {
+    "revision": "e5b2f99c96617b126977605ba5fe848c",
+    "url": "/static/media/bigslider_2.e5b2f99c.png"
+  },
+  {
+    "revision": "29a2232a306accedaead5ac6b4ff5e76",
+    "url": "/static/media/clock.29a2232a.svg"
+  },
+  {
+    "revision": "388696a3bd13fffde2452286883afca9",
+    "url": "/static/media/discover.388696a3.svg"
+  },
+  {
+    "revision": "0807d20315e81267a78f9697035a6881",
+    "url": "/static/media/fb.0807d203.svg"
+  },
+  {
+    "revision": "f459e503d3dcd1b0ebc347ec0946de01",
+    "url": "/static/media/globe.f459e503.svg"
+  },
+  {
+    "revision": "052c937f330a15af85871542d03fe107",
+    "url": "/static/media/home.052c937f.svg"
+  },
+  {
+    "revision": "0e65a7873a3f53bc8101dc22319fcae9",
+    "url": "/static/media/insta.0e65a787.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "147b3ca0b36f038dc5342dc5416bd547",
+    "url": "/static/media/mail.147b3ca0.svg"
+  },
+  {
+    "revision": "10f0942025efa5e29e47829ce6236266",
+    "url": "/static/media/news_bg.10f09420.png"
+  },
+  {
+    "revision": "2d35505665e6e7b72956254e8300c2a2",
+    "url": "/static/media/photos.2d355056.svg"
+  },
+  {
+    "revision": "8579c258a44d0f923134802bf857d5d2",
+    "url": "/static/media/send.8579c258.svg"
+  },
+  {
+    "revision": "ebff4a19cb3cd1c95eda727f20a7b039",
+    "url": "/static/media/smallslider_1.ebff4a19.png"
+  },
+  {
+    "revision": "718db5f8db05e7c1406f02590ba29fa9",
+    "url": "/static/media/smallslider_2.718db5f8.png"
+  },
+  {
+    "revision": "5f4dde5b21c9a82d89ff4ec25b7a161c",
+    "url": "/static/media/smallslider_3.5f4dde5b.png"
+  },
+  {
+    "revision": "98d3d3dbae7af09bc2e6975c00d1851a",
+    "url": "/static/media/trending_1.98d3d3db.png"
+  },
+  {
+    "revision": "b7d073a99d6fede93d0bce70ca78bbee",
+    "url": "/static/media/trending_2.b7d073a9.png"
+  },
+  {
+    "revision": "4f403975d98ee79c5a8d3c6e3b0ede7c",
+    "url": "/static/media/trending_3.4f403975.png"
+  },
+  {
+    "revision": "db08fa847a6290bd934ee7d8bf460505",
+    "url": "/static/media/twitter.db08fa84.svg"
+  }
+]);
