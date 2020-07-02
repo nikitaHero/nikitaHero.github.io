74b0c888a31b49806dfa2b34f01b1431ce559991
nikitaHero <46008598+nikitaHero@users.noreply.github.com>
2020-07-02 08:46:29 +0300

    deploy yalantis test app

---
+++ precache-manifest.1758e75f3c157607d31d4728f5f0a1b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba629af3ac9bbc881799a3dc4b5227d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e0020e57b902c695e2c",
+    "url": "/static/css/main.d0b5d67b.chunk.css"
+  },
+  {
+    "revision": "3bcf0b3f10f035e8df97",
+    "url": "/static/js/2.b2b2012e.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/static/js/2.b2b2012e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0020e57b902c695e2c",
+    "url": "/static/js/main.733891e8.chunk.js"
+  },
+  {
+    "revision": "2d030f83177ff6f1df05",
+    "url": "/static/js/runtime-main.14a9d4ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45df59786d7e7946fa936053676612dd.js"
+  "/precache-manifest.1758e75f3c157607d31d4728f5f0a1b9.js"
 );
 
 self.addEventListener('message', (event) => {
