ac0f8e0ae663c59f9d05a2cf12b9fdbb29e0093f
nikitaHero <ivanovnikita@protonmail.com>
2020-07-24 05:55:15 +0300

    s
    sss

---
+++ precache-manifest.de18532eafbef851971c8e7019a36f3f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb67d471f6dedda865881461bab0e5a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30a38b9a895795dff54d",
+    "url": "/static/css/main.ed221073.chunk.css"
+  },
+  {
+    "revision": "46feab0aad13694b9e9e",
+    "url": "/static/js/2.4b356bbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b356bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30a38b9a895795dff54d",
+    "url": "/static/js/main.a064f4fb.chunk.js"
+  },
+  {
+    "revision": "4ec9da3ee3509697f0d7",
+    "url": "/static/js/runtime-main.d67da714.js"
+  },
+  {
+    "revision": "f86b679131f04236642171cd8807d763",
+    "url": "/static/media/1-bg.f86b6791.jpg"
+  },
+  {
+    "revision": "4d69a63142c41259c04816c90cb61e4a",
+    "url": "/static/media/2-bg.4d69a631.jpg"
+  },
+  {
+    "revision": "ca4d4b82cd9ff542b4355956d3ee7173",
+    "url": "/static/media/coin.ca4d4b82.svg"
+  },
+  {
+    "revision": "a0c8701ebe9bc5c5cf98bc79f0b34f4f",
+    "url": "/static/media/hand-shake.a0c8701e.svg"
+  },
+  {
+    "revision": "62e347443c6c647361c6652872b4a1d8",
+    "url": "/static/media/logo.62e34744.png"
+  },
+  {
+    "revision": "f2e706764622eb78abca89e8225a53c8",
+    "url": "/static/media/old-fashion-briefcase-1.f2e70676.svg"
+  },
+  {
+    "revision": "73b3b14aa0bbd2bd173eb809d1a55562",
+    "url": "/static/media/pie-chart.73b3b14a.svg"
+  }
+]);
